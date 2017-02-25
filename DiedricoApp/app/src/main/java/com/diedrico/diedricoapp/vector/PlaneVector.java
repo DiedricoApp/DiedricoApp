9e025b41ef2a43f9419d32cf0bf55a0f54341c42
Fran Acién <amil101ftw@gmail.com>
2017-02-25 14:21:22 +0100

    making planeVector parcelable

--- DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/vector/PlaneVector.java
+++ DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/vector/PlaneVector.java
@@ -1,14 +1,49 @@
 package com.diedrico.diedricoapp.vector;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by amil101 on 22/07/16.
  */
-public class PlaneVector {
+public class PlaneVector implements Parcelable{
 
     private PointVector p1;
     private PointVector p2;
     private PointVector p3;
     private PointVector p4;
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeArray(new PointVector[]{
+                p1,
+                p2,
+                p3,
+                p4
+        });
+    }
+
+    public static final Parcelable.Creator<PlaneVector> CREATOR
+            = new Parcelable.Creator<PlaneVector>() {
+        public PlaneVector createFromParcel(Parcel in) {
+            return new PlaneVector(in);
+        }
+
+        public PlaneVector[] newArray(int size) {
+            return new PlaneVector[size];
+        }
+    };
+
+    private PlaneVector(Parcel in) {
+        PointVector pointVectors[] = (PointVector[]) in.readParcelableArray(PointVector.class.getClassLoader());
+        p1 = pointVectors[0];
+        p2 = pointVectors[1];
+        p3 = pointVectors[2];
+        p4 = pointVectors[3];
+    }
 
     public PlaneVector(PointVector p1, PointVector p2, PointVector p3, PointVector p4){
         this.p1 = p1;
