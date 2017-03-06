6eb654ac9daf6df22d65686353c398d671bf8541
Fran Acién <amil101ftw@gmail.com>
2017-03-06 15:16:14 +0100

    changing the way that thresholding works, now it only change the bitmap and returns the result

--- DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/picToDiedrico/Thresholding.java
+++ DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/picToDiedrico/Thresholding.java
@@ -5,6 +5,11 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 
+import com.diedrico.diedricoapp.vector.LineVector;
+import com.diedrico.diedricoapp.vector.PointVector;
+
+import java.util.List;
+
 import boofcv.alg.filter.binary.GThresholdImageOps;
 import boofcv.android.ConvertBitmap;
 import boofcv.android.VisualizeImageData;
@@ -15,34 +20,38 @@ import boofcv.struct.image.ImageUInt8;
  * Created by amil101 on 24/01/16.
  */
 public class Thresholding extends AsyncTask<Integer, Integer, Bitmap> {
-    ImageView imageView;
-    String pic;
+    ImageFloat32 input;
+    Bitmap scaledBitmap;
 
-    public Thresholding(ImageView imageView, String pic) {
+    public Thresholding.AsyncResponse delegate = null;
+
+    public Thresholding(Bitmap bitmap, AsyncResponse delegate) {
         super();
-        this.imageView = imageView;
-        this.pic = pic;
+
+        this.delegate = delegate;
+        scaledBitmap = Bitmap.createScaledBitmap(bitmap, 500, 500, false);      //Scale the bitmap to not crash
+
+        // convert into a usable format
+        input = ConvertBitmap.bitmapToGray(scaledBitmap,(ImageFloat32) null,null);                    //(image, null, ImageFloat32.class)
     }
 
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         super.onPostExecute(bitmap);
-
-        imageView.setImageBitmap(bitmap);
+        delegate.processFinish(bitmap);
     }
 
     @Override
     protected Bitmap doInBackground(Integer... params) {
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeFile(pic), 500, 500, false);      //Scale the bitmap to not crash
-
-        // convert into a usable format
-        ImageFloat32 input = ConvertBitmap.bitmapToGray(scaledBitmap,(ImageFloat32) null,null);                    //(image, null, ImageFloat32.class);
-
         ImageUInt8 binary = new ImageUInt8(input.width,input.height);
 
         GThresholdImageOps.localSauvola(input, binary, params[0], 0.3f, false);
         VisualizeImageData.binaryToBitmap(binary, false, scaledBitmap, null);
 
         return scaledBitmap;
+    }
+
+    public interface AsyncResponse{
+        void processFinish(Bitmap result);
     }
 }
