f0b600708d158388743c0f3d7c534eed1f58b8c9
Fran Acién <amil101ftw@gmail.com>
2017-04-05 00:20:28 +0200

    adding GPL header

--- DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/ViewPagerAdapter.java
+++ DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/ViewPagerAdapter.java
@@ -1,3 +1,21 @@
+/*Author: Fran Acién
+* Copyright (C) 2017  Fran Acién
+*
+* This file is part of DiedricoApp.
+*
+* DiedricoApp is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License.
+*
+* DiedricoApp is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.diedrico.diedricoapp;
 
 import android.content.res.Resources;
@@ -8,9 +26,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Dimitry Ivanov on 21.08.2015.
- */
 class ViewPagerAdapter extends FragmentPagerAdapter {
 
     private final List<Fragment> mFragmentList = new ArrayList<>();
--- DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/picToDiedrico/LineDiedrico.java
+++ DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/picToDiedrico/LineDiedrico.java
@@ -1,3 +1,21 @@
+/*Author: Fran Acién
+* Copyright (C) 2017  Fran Acién
+*
+* This file is part of DiedricoApp.
+*
+* DiedricoApp is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License.
+*
+* DiedricoApp is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.diedrico.diedricoapp.picToDiedrico;
 
 import com.diedrico.diedricoapp.vector.LineVector;
