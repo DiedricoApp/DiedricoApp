cd54cd92111d55b4740107d5bb34ce3212c37d9c
Fran Acién <amil101ftw@gmail.com>
2017-05-03 12:28:40 +0200

    adding method for model's coordinates

--- DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/opengl/models/Model.java
+++ DiedricoApp/app/src/main/java/com/diedrico/diedricoapp/opengl/models/Model.java
@@ -1,10 +1,13 @@
 package com.diedrico.diedricoapp.opengl.models;
 
+import com.diedrico.diedricoapp.vector.PointVector;
+
 /**
  * Created by amil101 on 3/08/16.
  */
 public interface Model {
+    public PointVector getCoords();
     public int getNumVerts();
-
     public float[] getVerts();
+
 }
